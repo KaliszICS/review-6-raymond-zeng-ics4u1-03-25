0f0af54dd230a6838d43aff4108db9f3b130c741
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-02-12 20:12:14 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,37 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-
+		
 	}
+	public static int sum(int num1, int num2){
 
+	
+		return (num1 + num2);
+	}
+	public static int difference(int num1, int num2){
+
+		return (num1 - num2);
+	}
+	public static double product(double num1, double num2){ 
+		return (num1 * num2);
+		
+	}
+	public static String removeFirst(String text){
+		return (String)(text.substring(1));
+	}
+	public static int combinedLength(String text1, String text2){
+		return (text1.length() + text2.length());
+	}
+	public static Boolean isEven(int num){
+		return (num % 2 == 0);
+	}
+	public static Boolean isOdd(int num){
+		return (num % 2 != 0);
+	}
+	public static Boolean isPositive(int num){
+		return (num > 0);
+	}
+	public static Boolean isNegative(int num){
+		return (num < 0);
+	}
 }
